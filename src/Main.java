d9dfc01c46fdd9014c9503baff16eaf7e6d75c2b
tjdanqkr <tjdanqkr@gmail.com>
2024-09-27 10:48:11 +0900

    test

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 
 public class Main {
     public static int i = 0;
-    public static void main(String[] args) {
-        for (int j = 0; j < 5; j++) {
-            Thread thread = new Test();
+    public static void main(String[] args) throws InterruptedException {
+        for (int j = 0; j < 100; j++) {
+            Thread thread = new Test(j);
             thread.start();
         }
+        Thread.sleep(2000);
+        System.out.println(Test.i); // 100
     }
 }
 
 class Test extends Thread {
-
+    public static int i = 0;
     @Override
     public void run() {
         try {
@@ -18,6 +20,16 @@ class Test extends Thread {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("쓰레드");
+        plusI();
+        System.out.println(j+"쓰레드");
+    }
+    public synchronized void plusI(){
+        i++;
+    }
+
+
+    public int j;
+    public Test(int j){
+        this.j = j;
     }
 }
